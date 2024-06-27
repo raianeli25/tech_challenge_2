512c761fc5f49204c9a698d3ea4fabbef25a0dee
Raiane Lima dos Santos <raiane.lima@clear.sale>
2024-06-27 13:12:47 -0300

    add new columns on data

--- download_csv.py
+++ download_csv.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 import time
 
 def download():
@@ -8,6 +10,17 @@ def download():
     try:
 
         driver.get('https://sistemaswebb3-listados.b3.com.br/indexPage/day/IBOV?language=pt-br')
+
+        webwait = WebDriverWait(driver, 10)
+
+        element = webwait.until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, '//*[@id="segment"]/option[2]')
+                )
+            )
+        element.click()
+        driver.execute_script("arguments[0]", element)
+        time.sleep(10)
 
         downloadcsv= driver.find_element(By.LINK_TEXT, "Download")
         
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime import date
 #download csv file from ibov site
 download()
 
-file_path = 'download path'
+file_path = 'C:\\Users\\raiane.lima\\Downloads'
 #convert file from csv to parquet
 today = date.today()
 today_str = today.strftime('%d-%m-%Y')
@@ -14,7 +14,7 @@ data = today_str[:-4]+today_str[-2:]
 df = pd.read_csv(file_path+'\\IBOVDia_'+data+'.csv', index_col=False, skiprows=[0], sep=';', encoding='latin-1', engine= 'python')
 df.drop(df.tail(2).index,inplace=True) 
 df['date'] = today_str
-df = df.rename(columns={"Código": "cod_acao", "Ação": "nome_acao", "Qtde. Teórica": "qtd_acao", "Part. (%)": "part"})
+df = df.rename(columns={"Setor":"setor_acao","Código": "cod_acao", "Ação": "nome_acao", "Qtde. Teórica": "qtd_acao", "Part. (%)": "part", "Part. (%)Acum.": "part_acum"})
 print(df.head())
 print(df.shape)
 file_name = 'dados_ibov_'+data+'.parquet'
@@ -22,10 +22,10 @@ df.to_parquet(file_name)
 
 #upload file on bucket s3
 handle_s3(file_name,\
-         'bucket_name',\
-         'aws_access_key_id',\
-         'aws_secret_access_key',\
-         'aws_session_token',\
+         'tech-challenge-2',\
+         '',\
+         '',\
+         '',\
          'upload',\
          file_name,\
          'raw_data')

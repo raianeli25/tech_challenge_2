65752a45261c9b0bfd5eec7b61672d877c5d6fd9
Raiane Lima dos Santos <raiane.lima@clear.sale>
2024-07-15 21:27:35 -0300

    update job glue

--- glue_job.py
+++ glue_job.py
@@ -26,10 +26,10 @@ job.init(args['JOB_NAME'], args)
 AmazonS3_node1719265558667 = glueContext.create_dynamic_frame.from_options(format_options={}, connection_type="s3", format="parquet", connection_options={"paths": ["s3://tech-challenge-2/raw_data/"], "recurse": True}, transformation_ctx="AmazonS3_node1719265558667")
 
 # Script generated for node Aggregate
-Aggregate_node1719267560128 = sparkAggregate(glueContext, parentFrame = AmazonS3_node1719265558667, groups = ["date", "setor_acao", "cod_acao"], aggs = [["qtd_acao", "sum"]], transformation_ctx = "Aggregate_node1719267560128")
+Aggregate_node1719267560128 = sparkAggregate(glueContext, parentFrame = AmazonS3_node1719265558667, groups = ["date", "cod_acao"], aggs = [["qtd_acao", "max"]], transformation_ctx = "Aggregate_node1719267560128")
 
 # Script generated for node Rename Field
-RenameField_node1719270814013 = RenameField.apply(frame=Aggregate_node1719267560128, old_name="`sum(qtd_acao)`", new_name="qtd_acao_por_setor_e_data", transformation_ctx="RenameField_node1719270814013")
+RenameField_node1719270814013 = RenameField.apply(frame=Aggregate_node1719267560128, old_name="`max(qtd_acao)`", new_name="qtd_max_acao", transformation_ctx="RenameField_node1719270814013")
 
 # Script generated for node Rename Field
 RenameField_node1719272698535 = RenameField.apply(frame=RenameField_node1719270814013, old_name="cod_acao", new_name="codigo_acao", transformation_ctx="RenameField_node1719272698535")
@@ -42,7 +42,7 @@ DerivedColumn_node1719273593759 = RenameField_node1719273626537.gs_derived(colNa
 
 # Script generated for node Amazon S3
 AmazonS3_node1719274399403 = glueContext.getSink(path="s3://tech-challenge-2/refined/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=["dia_mes_ano", "codigo_acao"], enableUpdateCatalog=True, transformation_ctx="AmazonS3_node1719274399403")
-AmazonS3_node1719274399403.setCatalogInfo(catalogDatabase="tech-challenge",catalogTableName="pregao_com_data_e_setor")
+AmazonS3_node1719274399403.setCatalogInfo(catalogDatabase="tech-challenge",catalogTableName="pregao_com_data")
 AmazonS3_node1719274399403.setFormat("glueparquet", compression="snappy")
 AmazonS3_node1719274399403.writeFrame(DerivedColumn_node1719273593759)
 job.commit()
